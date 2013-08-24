e4be0a628e9d50edab8714b4545f6e3a723c7674
Tessa Thornton <tessathornton@gmail.com>
2013-08-24 19:28:52 -0400

    move wp_head to end of head tag

--- header.php
+++ header.php
@@ -32,7 +32,6 @@ use_existing_jquery=false,
 f=false,d=document;return{use_existing_jquery:function(){return use_existing_jquery;},library_tolerance:function(){return library_tolerance;},finish:function(){if(!f){f=true;var a=d.getElementById('_vis_opt_path_hides');if(a)a.parentNode.removeChild(a);}},finished:function(){return f;},load:function(a){var b=d.createElement('script');b.src=a;b.type='text/javascript';b.innerText;b.onerror=function(){_vwo_code.finish();};d.getElementsByTagName('head')[0].appendChild(b);},init:function(){settings_timer=setTimeout('_vwo_code.finish()',settings_tolerance);this.load('//dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(d.URL)+'&r='+Math.random());var a=d.createElement('style'),b='body{opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important;}',h=d.getElementsByTagName('head')[0];a.setAttribute('id','_vis_opt_path_hides');a.setAttribute('type','text/css');if(a.styleSheet)a.styleSheet.cssText=b;else a.appendChild(d.createTextNode(b));h.appendChild(a);return settings_timer;}};}());_vwo_settings_timer=_vwo_code.init();
 </script>
 <!-- End Visual Website Optimizer Asynchronous Code -->
-  <?php wp_head(); ?>
 
   <meta property="og:title" content="Do you know the most important word in business?">
   <meta property="og:url" content="http://thefoundation.com/">
@@ -50,6 +49,7 @@ f=false,d=document;return{use_existing_jquery:function(){return use_existing_jqu
   ga('send', 'pageview');
 
 </script>
+  <?php wp_head(); ?>
 
 </head>
 
