99d11265cb83a3997a00043204f169860ff9d5e8
jamescosta <james@thephuse.com>
2013-08-25 22:07:35 -0400

    Adds group widget to thank you page

--- thankyou-page.php
+++ thankyou-page.php
@@ -18,6 +18,19 @@ Template Name: Thank You Page
   <h1 class="center">You are Almost Done.</h1>
   <h2 class="center">Check Your Email Now To Confirm Your Application</h2>
 
+  <div class="center">
+	  <div id="fb-root"></div>
+	  <script>(function(d, s, id) {
+	    var js, fjs = d.getElementsByTagName(s)[0];
+	    if (d.getElementById(id)) return;
+	    js = d.createElement(s); js.id = id;
+	    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=209982189069231";
+	    fjs.parentNode.insertBefore(js, fjs);
+	  }(document, 'script', 'facebook-jssdk'));</script>
+	  
+	  <div class="fb-like-box" data-href="https://www.facebook.com/TheFoundation.io" data-width="400" data-show-faces="true" data-header="true" data-stream="true" data-show-border="true"></div>
+  </div>
+
   <div class="row">
 
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
