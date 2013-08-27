c85f0dd3cce1dee344116ce343b50b31679016b4
Tessa Thornton <tessathornton@gmail.com>
2013-08-27 10:38:45 -0400

    upate readme

--- action-guide-page.php
+++ action-guide-page.php
@@ -12,35 +12,31 @@ Template Name: Action Guides Page
 <div class="container scroll-page row">
 
   <h1><?php the_title(); ?></h1>
-  
+
   <?php
   $ag_args = array(
     'post_type' => 'action-guide'
   );
   $action_guides = new WP_Query($ag_args);
-
   if ($action_guides->have_posts()) {while ($action_guides->have_posts()) {$action_guides->the_post();
     $cover_image = get_post_meta( $post->ID, '_cmb_cover_image', true );
-    $download_link = get_post_meta( $post->ID, '_cmb_pdf_download', true );
-  ?>
+    $download_link = get_post_meta( $post->ID, '_cmb_pdf_download', true ); ?>
 
-  <ul class="action-guides">
-    <li class="media">
-      <div class="media-image">
-        <img src="<?php echo $cover_image; ?>" />        
-      </div>
-      <div class="media-content">
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
-        <a class="btn-red ga_click" rel="action-guide;download;<?php echo basename($download_link); ?>" href="<?php echo $download_link; ?>" download>Download</a>        
-      </div>
-    </li>
-  </ul>
-
+    <ul class="action-guides">
+      <li class="media">
+        <div class="media-image">
+          <img src="<?php echo $cover_image; ?>" />
+        </div>
+        <div class="media-content">
+          <h2><?php the_title(); ?></h2>
+          <?php the_content(); ?>
+          <a class="btn-red ga_click" rel="action-guide;download;<?php echo basename($download_link); ?>" href="<?php echo $download_link; ?>" download>Download</a>
+        </div>
+      </li>
+    </ul>
 
   <?php }} ?>
 
 </div>
 
 <?php get_footer(); ?>
-  
--- readme.md
+++ readme.md
@@ -1,24 +1,11 @@
 The Foundation Theme
 ====================
-##WordPress things that will be needed: 
 
-- Custom Post Types
-    - Components: 
-        - Home page hero
-        - get free case study
-  - Press features
-  - Case studies
-  - Testimonials
-  - Podcasts
-- Page templates
-    - Home page
-- Custom Archives
-    - podcast archive
-    - case study archive
-- Custom Single
-    - podcast
-    - case study
-- Custom Widgets
-    - Join the foundation
-    - component: get the case study
-    - resources
+Watch and compile .scss files to css:
+
+```
+cd styles
+sass --watch main.scss:main.css
+```
+
+HTML code for application form comes from Office Autopilot. Form code is in [this gist](https://gist.github.com/tessalt/f11d652cfd88ba86102e). Must be minified before being pasted into WordPress.
