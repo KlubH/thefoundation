0186c34a8b8b429999f36004668282ea7d35294d
MichelleTodd <himichelletodd@gmail.com>
2013-09-10 17:59:32 -0400

    Makes nav fixed on homepage (styled to be like other navs)

--- home-page.php
+++ home-page.php
@@ -5,7 +5,7 @@ Template Name: Home Page
 ?>
 <?php get_header(); ?>
 
-<header class="site-header fixed-top">
+<header class="site-header scroll-nav">
   <?php get_template_part( 'nav' ); ?>
 </header>
 
